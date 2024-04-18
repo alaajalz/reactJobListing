8b943e382888e0005008c9339d258e1f35aea699
unknown <alagie.maj@gmail.com>
2024-04-18 15:15:17 +0000

    Initial Commit

---
+++ src/pages/JobsPage.jsx
@@ -0,0 +1,11 @@
+import JobListings from "../components/JobListings";
+
+const JobsPage = () => {
+    return (
+        <section className="">
+            <JobListings isHome={false} />
+        </section>
+    )
+}
+
+export default JobsPage;
